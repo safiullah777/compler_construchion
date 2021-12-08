d2c75c0dfcb91e54f648bd053b0fcd7d5fda2453
safiullah777 <57296932+safiullah777@users.noreply.github.com>
2021-12-08 18:41:08 +0500

    e
    e

--- syntax.js
+++ syntax.js
@@ -3,4 +3,14 @@ const data = fs.readFileSync("output.txt", { encoding: "utf-8" }).split("\n")
 const tokens =data.slice(0,data.length-1).map(data=>{
     return JSON.parse(data)
 })
-console.log(tokens)
+var DT=['var','const','let'];
+var i=0;
+const declaration =(newtoken)=>{
+    if(DT.includes(newtoken.valuepart)){
+        i++
+        if(tokens[i].classpart=="Id"){
+            console.log(1);
+        }
+    }
+}
+declaration(tokens[i])
